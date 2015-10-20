1f5583ef6e410c6911f95b6cf15d61327b6a606b
Lev Eidelman Nagar <lev@techsee.me>
2015-10-20 15:14:56 +0300

    no message

--- app.js
+++ app.js
@@ -6,9 +6,12 @@
 *
 * Description
 */
-angular.module('ScheduleApp', ['firebase'])
+angular.module('scheduleApp', ['firebase'])
 
 	// application main controller
 	.controller('mainController', function ($scope, $firebase){
 
+		// connect to firebase
+		var ref = new Firebase("https://scorching-fire-1003.firebaseio.com/days");
+		var fb = $firebase(ref);
 	});
