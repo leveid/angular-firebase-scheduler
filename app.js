f5a559082222b832ce3794ba5e94c4b07b5f3655
Lev Eidelman Nagar <lev@techsee.me>
2015-10-20 14:42:40 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// app.js
+
+/**
+* ScheduleApp Module
+*
+* Description
+*/
+angular.module('ScheduleApp', ['firebase'])
+
+	// application main controller
+	.controller('mainController', function ($scope, $firebase){
+
+	});
